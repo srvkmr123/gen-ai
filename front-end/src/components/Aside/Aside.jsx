3f98acf75f6ad52950f294e8d02176d9fa424acf
sourav kumar <srvdhiman1998@gmail.com>
2023-12-22 13:52:06 +0530

    added some changes

--- front-end/src/components/Aside/Aside.jsx
+++ front-end/src/components/Aside/Aside.jsx
@@ -1,9 +1,30 @@
 import './Aside.scss'
+import { Link } from "react-router-dom";
+import FilesUpload from '../FilesUpload/FilesUpload';
 
 function Aside() {
   return (
     <div className='aside'>
-        Aside
+        <h1>Gen AI</h1>
+        <div className="aside__use-cases">
+            <h2>Use Cases</h2>
+        <ul>
+            <li><Link to=''>Business rule extraction</Link></li>
+            <li><Link to=''>API discoverability</Link></li>
+            <li><Link to=''>Use best practices for API</Link></li>
+        </ul>
+        </div>
+        <div className="aside__pinecode-indexes">
+            <h2>Pinecode Indexes</h2>
+        <ul>
+            <li><Link to=''>Extract biz rule</Link></li>
+            <li><Link to=''>DiscoveApi</Link></li>
+            <li><Link to=''>API best practices</Link></li>
+        </ul>
+        </div>
+        <div className="aside__filesupload-btn">
+           <FilesUpload/>
+        </div>
     </div>
   )
 }
---
+++ front-end/src/components/FilesUpload/FilesUpload.jsx
@@ -0,0 +1,54 @@
+import {useState} from 'react'
+import './FilesUpload.scss'
+
+function FilesUpload() {
+    const [files, setFiles] = useState([])
+    
+
+    const uploadFiles = (data)=> {
+       data.forEach(d=>console.log(d))
+    }
+
+    const packFiles = (files)=> {
+        const data = new FormData();
+
+        [...files].forEach((file, i) => {
+            data.append(`file-${i}`, file, file.name)
+        })
+        return data
+    }
+
+    const handleUploadClick = () => {
+        if (files.length) {
+            const data = packFiles(files)
+            uploadFiles(data)
+        }
+    }
+
+    const renderFileList = () => (<ol>
+        {[...files].map((f, i) => (
+            <li key={i}>{f.name} - {f.type}</li>
+        ))}
+    </ol>)
+
+    // const renderButtonStatus = () => (
+    //     (status === STATUS_IDLE) ? 
+    //         'Send to server' : 
+    //         <img src = "./load.svg" />
+    // )
+
+    return (<div>
+        <input
+            type="file"
+            accept="*" 
+            multiple
+            className='choose-file'
+            onChange={(e)=> setFiles(e.target.files)} />
+        {renderFileList()}
+        <button id='file-upload-btn' onClick={handleUploadClick}>
+           Upload
+        </button>
+    </div>)
+}
+
+export default FilesUpload
