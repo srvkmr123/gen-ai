4642396f5f055c07b8861ac8e2a651781b803d80
sourav kumar <srvdhiman1998@gmail.com>
2023-12-22 20:43:51 +0530

    filepond for file upload

--- front-end/src/components/FilesUpload/FilesUpload.jsx
+++ front-end/src/components/FilesUpload/FilesUpload.jsx
@@ -1,7 +1,8 @@
 import {useState} from 'react'
 import './FilesUpload.scss'
 import { IoMdAttach } from "react-icons/io";
-
+import { FilePond } from 'react-filepond';
+import 'filepond/dist/filepond.min.css';
 
 function FilesUpload() {
     const [files, setFiles] = useState([])
@@ -27,11 +28,7 @@ function FilesUpload() {
         }
     }
 
-    const renderFileList = () => (<ol className='files-ol'>
-        {[...files].map((f, i) => (
-            <li className='files-list' key={i}>{f.name.substring(0,10)} - {f.type}</li>
-        ))}
-    </ol>)
+    
 
     // const renderButtonStatus = () => (
     //     (status === STATUS_IDLE) ? 
@@ -42,20 +39,26 @@ function FilesUpload() {
     return (<div>
 
         {/* for multiple files upload */}
-        <input
-            type="file"
-            accept="*" 
-            multiple
-            className='choose-file'
-            onChange={(e)=> setFiles(e.target.files)} />
-        {renderFileList()}
+        <FilePond
+        className='file-upload'
+                files={files}
+                onupdatefiles={setFiles}
+                allowMultiple={true}
+                maxFiles={10}
+                allowDrop
+                webkitdirectory
+                instantUpload={false}
+                server="https://httpbin.org/post"
+                name="files"
+                labelIdle='Drag & Drop your files or <span class="filepond--label-action">Browse</span>'
+            />
 
         {/* for folder upload only */}
-        <label htmlFor="choose-folder" className='select-folder'>Choose folder <IoMdAttach size={20}/></label>
+        {/* <label htmlFor="choose-folder" className='select-folder'>Choose folder <IoMdAttach size={20}/></label>
         <input  id='choose-folder' webkitdirectory="" type="file" hidden />
         <button className='file-upload-btn' onClick={handleUploadClick}>
         <i className="fa-solid fa-arrow-up-from-bracket"></i> Upload 
-        </button>
+        </button> */}
     </div>)
 }
 
