1bcc4a2a2ff2a269c679290586437751008752e4
sourav kumar <srvdhiman1998@gmail.com>
2023-12-26 13:53:08 +0530

    browse button style

--- front-end/src/components/FilesUpload/FilesUpload.jsx
+++ front-end/src/components/FilesUpload/FilesUpload.jsx
@@ -1,5 +1,5 @@
 
-import React from 'react';
+import React,{useRef} from 'react';
 import Uppy from '@uppy/core';
 import { Dashboard } from '@uppy/react';
 import '@uppy/core/dist/style.min.css';
@@ -8,22 +8,26 @@ import XHR from '@uppy/xhr-upload';
 import './FilesUpload.scss'
 
  const FileUpload = () => {
+
+  const dashboardRef = useRef(null);
+
   const uppy = React.useMemo(() => {
     return new Uppy({
     debug:true,
       restrictions: { maxNumberOfFiles: 25 },
       autoProceed: false,
       
-    }).use(XHR, { endpoint: "https://httpbin.org/post"})
+    })
+    .use(XHR, { endpoint: "https://httpbin.org/post"})
     .on('complete',result=>{
-      // console.log(res);
       result.successful.forEach((file) => {
         const responseData = file.response;
         console.log('Response data for file', file.id, responseData);
       });
     })
   }, []);
-	return <Dashboard className='uppy-dashboard' uppy={uppy} plugins={[]} width='250px' height='250px' proudlyDisplayPoweredByUppy={false} />
+  
+	return <Dashboard ref={dashboardRef} className='uppy-dashboard'  uppy={uppy}  width='250px' height='250px' proudlyDisplayPoweredByUppy={false} target='#uppy-dashboard'/>
    
 };
 export default FileUpload 
